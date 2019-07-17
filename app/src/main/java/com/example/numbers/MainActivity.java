e74d8e99e95b6a72027f4434cc1c339befaf1c74
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-07-16 21:46:19 -0400

    add

--- app/src/main/java/com/example/numbers/MainActivity.java
+++ app/src/main/java/com/example/numbers/MainActivity.java
@@ -3,27 +3,80 @@ package com.example.numbers;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    int total;
+    ArrayList<Integer> i;
+    int num;
+    TextInputEditText in;
+    TextView t1;
+    TextView t2;
+    TextView t3;
+    TextView t4;
+    TextView t5;
+    TextView t6;
+    TextView t7;
+    TextView t8;
+    TextView t9;
+    TextView t10;
+    TextView totaltext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        in=findViewById(R.id.b);
+        i=new ArrayList<Integer>();
+        Button Add = findViewById(R.id.button);
+        Button Clear = findViewById(R.id.button2);
+        t1=findViewById(R.id.textView);
+        t2=findViewById(R.id.textView2);
+        t3=findViewById(R.id.textView3);
+        t4=findViewById(R.id.textView4);
+        t5=findViewById(R.id.textView5);
+        t6=findViewById(R.id.textView6);
+        t7=findViewById(R.id.textView7);
+        t8=findViewById(R.id.textView8);
+        t9=findViewById(R.id.textView9);
+        t10=findViewById(R.id.textView10);
+        totaltext=findViewById(R.id.textView11);
+        Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                total+=Integer.parseInt(in.getText().toString());
+                i.add(Integer.parseInt(in.getText().toString()));
+                num+=1;
+                t10.setText(t9.getText());
+                t9.setText(t8.getText());
+                t8.setText(t7.getText());
+                t7.setText(t6.getText());
+                t6.setText(t5.getText());
+                t5.setText(t4.getText());
+                t4.setText(t3.getText());
+                t3.setText(t2.getText());
+                t2.setText(t1.getText());
+                t1.setText(in.getText());
+                in.setText("");
+                totaltext.setText(Integer.toString(total));
+            }
+        });
+
+
+        Clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
